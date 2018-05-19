336013f88210867a0bef9c0912dae68c7268750f
Anwesh43 <anweshthecool0@gmail.com>
2018-05-20 02:01:58 +0530

    adding smileyfaceview module

---
+++ smileyfaceview/src/main/java/com/example/smileyfaceview/SmileyFaceView.kt
@@ -0,0 +1,11 @@
+package com.example.smileyfaceview
+
+/**
+ * Created by anweshmishra on 20/05/18.
+ */
+
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+
