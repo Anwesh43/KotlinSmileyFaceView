8e9215e6c9d37a2329d968d8bd8f262b2164e9e6
Anwesh43 <anweshthecool0@gmail.com>
2018-05-20 02:04:17 +0530

    adding SmileyFaceView class

--- smileyfaceview/src/main/java/com/example/smileyfaceview/SmileyFaceView.kt
+++ smileyfaceview/src/main/java/com/example/smileyfaceview/SmileyFaceView.kt
@@ -9,3 +9,20 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.*
 
+class SmileyFaceView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
