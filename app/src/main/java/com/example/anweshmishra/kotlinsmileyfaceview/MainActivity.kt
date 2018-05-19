ec7f2fad0eb0d958c24d78856c41b8a973824414
Anwesh43 <anweshthecool0@gmail.com>
2018-05-20 02:53:06 +0530

    adding static create method

--- app/src/main/java/com/example/anweshmishra/kotlinsmileyfaceview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinsmileyfaceview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.anweshmishra.kotlinsmileyfaceview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smileyfaceview.SmileyFaceView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        SmileyFaceView.create(this)
     }
 }
