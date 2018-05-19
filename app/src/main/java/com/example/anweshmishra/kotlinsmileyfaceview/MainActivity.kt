b99ba07093347e74302c6e2c55e0d79a78d21669
Anwesh43 <anweshthecool0@gmail.com>
2018-05-20 03:29:11 +0530

    adding fullscreen support

--- app/src/main/java/com/example/anweshmishra/kotlinsmileyfaceview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinsmileyfaceview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.anweshmishra.kotlinsmileyfaceview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.example.smileyfaceview.SmileyFaceView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SmileyFaceView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
