d552f19deacbf6bb75f8ac83f597773b41054105
Anwesh43 <anweshthecool0@gmail.com>
2018-05-20 11:49:28 +0530

    adding OnSmileyFaceListener in MainActivity

--- app/src/main/java/com/example/anweshmishra/kotlinsmileyfaceview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinsmileyfaceview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.anweshmishra.kotlinsmileyfaceview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.smileyfaceview.SmileyFaceView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SmileyFaceView.create(this)
+        var view : SmileyFaceView = SmileyFaceView.create(this)
         fullScreen()
+        view.addOnSmileyFaceListener({
+            Toast.makeText(this, "I am happy so I am visible", Toast.LENGTH_SHORT).show()
+        }, {
+            Toast.makeText(this, "I am sad so I left the room", Toast.LENGTH_SHORT).show()
+        })
     }
 }
 fun MainActivity.fullScreen() {
